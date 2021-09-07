83fbfe3eb60710ea6c9b7b0db19ffe39746cdacd
Enrique-N <enrique.xe@gmail.com>
2021-09-07 18:31:19 -0500

    Rendering update

--- src/Components/CartContainer/Cartcontainer.js
+++ src/Components/CartContainer/Cartcontainer.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { Link } from 'react-router-dom';
 import { CartContext } from '../../CartContext';
 import CartItems from "../CartItems/CartItems";
 
@@ -15,9 +16,16 @@ const Cartcontainer = () => {
         <div>
             <CartItems />
             <div className="d-flex justify-content-center my-3 fs-4" style={{ marginLeft: "35%" }}>Total: ${totalPrice}</div>
-            <div className="d-flex justify-content-center" style={{ margin: "70px 0" }}>
-                <button type="button" className="btn btn-dark " style={{ height: "50px" }} onClick={Clear}>Clear</button>
-            </div>
+            {cartItems.length == 0
+                ? <div className="d-flex justify-content-center" style={{ margin: "70px 0" }}>
+                    <Link to="/">
+                        <button type="button" className="btn btn-dark " style={{ height: "50px" }}>Volver</button>
+                    </Link>
+                </div>
+                : <div className="d-flex justify-content-center" style={{ margin: "70px 0" }}>
+                    <button type="button" className="btn btn-dark " style={{ height: "50px" }} onClick={Clear}>Clear</button>
+                </div>
+            }
         </div>
 
     )
